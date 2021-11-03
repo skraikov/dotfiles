5567405f1d240a8ea4d873df6921b0b9c776d1a8
Serge Kraikov <serge.kraikov@gmail.com>
2021-11-03 08:59:55 +0500

    utils: minor exifcopy fix

--- .local/bin/exifcopy/exifcopy.py
+++ .local/bin/exifcopy/exifcopy.py
@@ -11,31 +11,32 @@ FILES = glob.glob("./**/*", recursive=True)
 for file in FILES:
     if not os.path.isfile(file):
         continue
-    metadata = pyexiv2.ImageMetadata(file)
-    metadata.read()
-    d = None
-    if 'Exif.Image.DateTime' in set(metadata.exif_keys):
-        d = metadata['Exif.Image.DateTime']
-    elif 'Iptc.Application2.DateCreated' in set(metadata.iptc_keys):
-        d = metadata['Iptc.Application2.DateCreated']
-    elif 'Xmp.xmp.ModifyDate' in set(metadata.xmp_keys):
-        d = metadata['Xmp.xmp.ModifyDate']
+    try:
+        file_basename = os.path.basename(file)
+        print(file, end = ": ")
+        metadata = pyexiv2.ImageMetadata(file)
+        metadata.read()
+        d = None
+        if 'Exif.Image.DateTime' in set(metadata.exif_keys):
+            d = metadata['Exif.Image.DateTime']
+        elif 'Iptc.Application2.DateCreated' in set(metadata.iptc_keys):
+            d = metadata['Iptc.Application2.DateCreated']
+        elif 'Xmp.xmp.ModifyDate' in set(metadata.xmp_keys):
+            d = metadata['Xmp.xmp.ModifyDate']
 
-    if d != None and isinstance(d.value, datetime.datetime):
-        date_dir = d.value.strftime("%Y-%m-%d")
-        month_dir = d.value.strftime("%Y-%m")
-        year_dir = d.value.strftime("%Y")
-        path = os.path.join(LIB_DIR, year_dir, month_dir, date_dir)
-        try:
-            file_basename = os.path.basename(file)
-            print(file + ' -> ' + date_dir + '/')
+        if d != None and isinstance(d.value, datetime.datetime):
+            date_dir = d.value.strftime("%Y-%m-%d")
+            month_dir = d.value.strftime("%Y-%m")
+            year_dir = d.value.strftime("%Y")
+            path = os.path.join(LIB_DIR, year_dir, month_dir, date_dir)
+            print(' -> ' + date_dir + '/')
             os.makedirs(path, exist_ok=True)
             new_name = os.path.join(path, file_basename)
             if os.path.exists(new_name):
                 raise Exception('File already exists!')
             shutil.move(file, new_name)
             os.chmod(new_name, 0o640)
-        except Exception as err:
-            print(err)
-    else:
-        print(file + ' UNKNOWN')
+        else:
+            print('UNKNOWN')
+    except Exception as err:
+        print(err)
