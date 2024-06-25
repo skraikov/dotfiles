6ad6d843c24e8de5ae45b1d290252587c0068987
Serge Kraikov <serge.kraikov@gmail.com>
2024-06-25 12:36:50 +0500

    gopass: fix enter-password for passwords with dollar sign

--- .local/bin/enter-password.sh
+++ .local/bin/enter-password.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-gopass ls --flat | rofi -dmenu | xargs --no-run-if-empty gopass show -o | sed 's/\(.\)/\1 /g' | sed 's/\([A-Z]\)/shift+\1/g' | sed 's/!/shift+1/g' | sed 's/[(]/shift+9/g' | xargs --no-run-if-empty xdotool key
+gopass ls --flat | rofi -dmenu | xargs --no-run-if-empty gopass show -o | \
+    sed 's/\(.\)/\1 /g' | sed 's/\([A-Z]\)/shift+\1/g' | sed 's/!/shift+1/g' | \
+    sed 's/[(]/shift+9/g' | sed 's/[$]/shift+4/g' | \
+    xargs --no-run-if-empty xdotool key
