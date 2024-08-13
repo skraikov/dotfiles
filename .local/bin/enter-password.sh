7678554542966f21ac8466917de2be195185aefb
Serge Kraikov <serge.kraikov@gmail.com>
2024-08-13 20:45:47 +0500

    gopass: support # in enter-password script

--- .local/bin/enter-password.sh
+++ .local/bin/enter-password.sh
@@ -2,5 +2,5 @@
 
 gopass ls --flat | rofi -dmenu | xargs --no-run-if-empty gopass show -o | \
     sed 's/\(.\)/\1 /g' | sed 's/\([A-Z]\)/shift+\1/g' | sed 's/!/shift+1/g' | \
-    sed 's/[(]/shift+9/g' | sed 's/[$]/shift+4/g' | \
+    sed 's/[(]/shift+9/g' | sed 's/[$]/shift+4/g' | sed 's/[#]/shift+3/g' | \
     xargs --no-run-if-empty xdotool key
