dc99e2fd03917a76d0189c29baa796deb276d5c5
Serge Kraikov <serge.kraikov@gmail.com>
2024-10-02 09:28:33 +0500

    awesome: fix theme

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -40,7 +40,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(awful.util.get_themes_dir() .. "default/theme.lua")
+beautiful.init("/home/serge/.config/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 -- terminal = "xterm"
