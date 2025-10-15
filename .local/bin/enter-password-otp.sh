ca731b6ea722b725e37b300ba991c61a430f6433
Serge Kraikov <serge.kraikov@gmail.com>
2025-10-15 14:46:50 +0500

    gopass: migrate enter-password scripts to ydotool

--- .local/bin/enter-password-otp.sh
+++ .local/bin/enter-password-otp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gopass ls --flat | rofi -dmenu | xargs --no-run-if-empty gopass otp -o | sed 's/\(.\)/\1 /g' | sed 's/\([A-Z]\)/shift+\1/g' | sed 's/!/shift+1/g' | xargs --no-run-if-empty xdotool key --delay 100
+gopass ls --flat | rofi -dmenu | xargs --no-run-if-empty gopass otp -o | ydotool type --key-delay 100 -f -
--- .local/bin/enter-password.sh
+++ .local/bin/enter-password.sh
@@ -2,25 +2,4 @@
 
 # where to find xdotool keykodes: https://gitlab.com/nokun/gestures/-/wikis/xdotool-list-of-key-codes
 
-gopass ls --flat | rofi -dmenu | xargs --no-run-if-empty gopass show -o | \
-    sed 's/\(.\)/\1 /g' | \
-    sed 's/\([A-Z]\)/shift+\1/g' | \
-    sed 's/!/shift+1/g' | \
-    sed 's/[$]/shift+4/g' | \
-    sed 's/[#]/shift+3/g' | \
-    sed 's/[-]/minus/g' | \
-    sed 's/[_]/underscore/g' | \
-    sed 's/  / space/g' | \
-    sed 's/[%]/percent/g' | \
-    sed 's/[<]/less/g' | \
-    sed 's/[>]/greater/g' | \
-    sed 's/[.]/period/g' | \
-    sed 's/[[]/bracketleft/g' | \
-    sed 's/[]]/bracketright/g' | \
-    sed 's/[{]/braceleft/g' | \
-    sed 's/[}]/braceright/g' | \
-    sed 's/[(]/parenleft/g' | \
-    sed 's/[)]/parenright/g' | \
-    sed 's/[&]/ampersand/g' | \
-    sed 's/[|]/bar/g' | \
-    xargs --no-run-if-empty xdotool key
+gopass ls --flat | rofi -dmenu | xargs --no-run-if-empty gopass show -o | ydotool type --key-delay 2 --key-hold 2 -f -
